9ff40c8fba6372ac3cab2c711ca40a603b43b1a1
coop-blake <96803255+coop-blake@users.noreply.github.com>
2023-10-06 15:22:39 -0700

    added odbc module to package, will replace vbs script

---
+++ src/Google/Inventory/odbc.ts
@@ -0,0 +1,23 @@
+const odbc = require("odbc");
+
+const query =
+  "SELECT " +
+  "TRIM(SI.INV_ScanCode) AS ScanCode," +
+  "TRIM(PCD_Price_pl1) AS Price, " +
+  "TRIM(DP.DIS_Description) AS Discount," +
+  "TRIM(PCD_WRKName) AS Worksheet, " +
+  "TRIM(PCD_PSWStartDate) AS StartDate, " +
+  "TRIM(PCD_PSWEndDate) AS EndDate " +
+  "FROM PriceChangeData " +
+  "LEFT JOIN   DiscountProfiles DP ON PCD_DIS_FK_pl1 = DP.DIS_PK AND PCD_DIS_CFK_pl1 = DP.DIS_CPK " +
+  "LEFT JOIN   StockInventory SI ON PCD_INV_FK = SI.INV_PK AND PCD_INV_CFK = SI.INV_CPK " +
+  "WHERE PCD_PSWEndDate > NOW();";
+
+const connection = odbc.connect("DSN=Prototype", (error, connection) => {
+  connection.query(query, (error, result) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log(result);
+  });
+});
