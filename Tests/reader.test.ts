db8cbbc9b2b2de17d7f79ae2546d7915bf4744ac
Blake Glanville <blake@firstalt.coop>
2023-10-11 01:24:11 -0700

    added tests and import

--- Tests/reader.test.ts
+++ Tests/reader.test.ts
@@ -1,34 +1,39 @@
 import { describe, expect, test } from "@jest/globals";
-import { StringsAssimilator, LineReader } from "../src/Readers/LineReader";
+import {
+  StringsAssimilator,
+  LineReader,
+  InputStream,
+  Importer
+} from "../src/Readers/LineReader";
+
+/* To use the entry assimilator, you need to define an entry */
+
+type TestEntry = {
+  name: string;
+  test: string;
+  value: number;
+};
+
+/* Then you define your Assimilator*/
+
+class TestAssimilator extends StringsAssimilator<TestEntry> {
+  digest() {
+    return this.raw.length === 3
+      ? {
+          name: this.raw[0],
+          test: this.raw[1],
+          value: parseInt(this.raw[2])
+        }
+      : null;
+  }
+}
+//give it to the LineReader
+class TestLineReader extends LineReader<TestEntry> {}
+
+const testLineReader = new TestLineReader(TestAssimilator);
 
 describe("Entry Assimilator", () => {
-  /* To use the entry assimilator, you need to define an entry */
-
-  type TestEntry = {
-    name: string;
-    test: string;
-    value: number;
-  };
-
-  /* Then you define your Assimilator*/
-
-  class TestAssimilator extends StringsAssimilator<TestEntry> {
-    digest() {
-      return this.raw.length === 3
-        ? {
-            name: this.raw[0],
-            test: this.raw[1],
-            value: parseInt(this.raw[2])
-          }
-        : null;
-    }
-  }
-  //give it to the LineReader
-  class TestLineReader extends LineReader<TestEntry> {}
-
-  const testLineReader = new TestLineReader(TestAssimilator);
-
-  test("Is it a ", async () => {
+  test("Does it assimilate", async () => {
     /* Get some raw data */
     const rawStrings = ["nameString", "This is a test", "10"];
     /* and Digest it to your type*/
@@ -44,7 +49,9 @@ describe("Entry Assimilator", () => {
     const testEntry = new TestAssimilator(rawStrings).digest();
     expect(testEntry).toBe(null);
   });
+});
 
+describe("Line Reader", () => {
   test("Read An Array of Array of Strings", async () => {
     const testLineRead = testLineReader.read([
       "nameString",
@@ -52,5 +59,40 @@ describe("Entry Assimilator", () => {
       "10"
     ]);
     expect(testLineRead?.test).toBe("This is a test");
+  });
+
+  test("Read To large an Array", async () => {
+    const testLineRead = testLineReader.read([
+      "nameString",
+      "This is a test",
+      "10",
+      "Another"
+    ]);
+    expect(testLineRead).toBe(null);
+  });
+  test("Read To small an Array", async () => {
+    const testLineRead = testLineReader.read(["nameString", "This is a test"]);
+    expect(testLineRead).toBe(null);
+  });
+});
+
+class TestInputStream implements InputStream {
+  getLines() {
+    return [
+      ["SD"],
+      ["nameString", "This is a test", "10", "Extra"],
+      ["nameString", "This is a test", "10"],
+      ["nameString", "This is a test", "10"]
+    ];
+  }
+}
+const testInputStream = new TestInputStream();
+describe("Importer", () => {
+  test("SHould have entries and invalid entries", async () => {
+    const importer = new Importer(testLineReader, testInputStream);
+    const entries = await importer.start();
+    console.log(entries);
+    expect(importer.getNumberOfEntries()).toBe(2);
+    expect(importer.getNumberOfUnrecognizedEntries()).toBe(2);
   });
 });
--- src/Readers/LineReader.ts
+++ src/Readers/LineReader.ts
@@ -1,35 +1,64 @@
-export interface LineReaderInterface<T> {
-  data: Array<Array<string>>;
-  entries: Array<T>;
-  unexpected: Array<T>;
-  processLine(line: string): boolean;
-}
-
-export interface AssimilatorInterface<T> {
+export abstract class StringsAssimilator<T> {
   raw: Array<string>;
-  digest(): T | null;
-}
-
-export abstract class StringsAssimilator<T> implements AssimilatorInterface<T> {
-  raw: Array<string>;
-
   constructor(raw: Array<string>) {
     this.raw = raw;
   }
-
   abstract digest(): T | null;
 }
 
 export abstract class LineReader<T> {
-  assimilator: new (raw: Array<string>) => AssimilatorInterface<T>;
-
-  constructor(
-    assimilator: new (raw: Array<string>) => AssimilatorInterface<T>
-  ) {
+  assimilator: new (raw: Array<string>) => StringsAssimilator<T>;
+  constructor(assimilator: new (raw: Array<string>) => StringsAssimilator<T>) {
     this.assimilator = assimilator;
   }
-
   read(raw: Array<string>): T | null {
     return new this.assimilator(raw).digest();
+  }
+}
+
+export abstract class InputStream {
+  constructor() {}
+  abstract getLines(): Array<Array<string>>;
+}
+
+export interface ImporterInterface<T> {
+  lineReader: LineReader<T>;
+  inputStream: InputStream;
+  start(): Promise<Array<T> | null>;
+}
+
+export class Importer<T> implements ImporterInterface<T> {
+  lineReader: LineReader<T>;
+  inputStream: InputStream;
+
+  entries = Array<T>();
+
+  unrecognizedInputs = Array<Array<string>>();
+
+  constructor(lineReader: LineReader<T>, inputStream: InputStream) {
+    this.inputStream = inputStream;
+    this.lineReader = lineReader;
+  }
+
+  async start(): Promise<Array<T>> {
+    const filtered: Array<T> = this.inputStream
+      .getLines()
+      .map((inStreamLine) => {
+        const entry = this.lineReader.read(inStreamLine) as T;
+        entry ?? !this.unrecognizedInputs.push(inStreamLine);
+        return entry;
+      })
+      .filter((line) => {
+        return line as T;
+      }) as Array<T>;
+    this.entries = filtered;
+    return filtered;
+  }
+
+  getNumberOfEntries() {
+    return this.entries.length;
+  }
+  getNumberOfUnrecognizedEntries() {
+    return this.unrecognizedInputs.length;
   }
 }
