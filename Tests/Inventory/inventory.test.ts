8833f510e065e97c07a22519bf1394d6e849515f
Blake Glanville <blake@firstalt.coop>
2023-10-17 11:05:54 -0700

    Added Inventory resource and Tests

--- Tests/Inventory/inventory.test.ts
+++ Tests/Inventory/inventory.test.ts
@@ -1,29 +1,39 @@
 import { describe, expect, test, beforeAll } from "@jest/globals";
-
+import { first } from "rxjs";
 import {
   Inventory,
-  InventoryStatus
+  Status as InventoryStatus
 } from "../../src/Resources/Inventory/Inventory";
 
 const inventory = new Inventory();
 
-beforeAll(async () => {});
+beforeAll(async () => {
+  //Get the Inventory State and wait until status becomes available
+  const state = inventory.getState();
+  const availablePromise = new Promise<void>((resolve) => {
+    state.status$
+      .pipe(
+        first((newStatus) => {
+          return newStatus === InventoryStatus.Available;
+        })
+      )
+      .subscribe(() => {
+        resolve();
+      });
+  });
+
+  await availablePromise;
+});
 describe("Inventory exists correctly", () => {
   test(`Should be Inventory class`, async () => {
     expect(inventory).toBeInstanceOf(Inventory);
   });
-
-  test(`Should be able to get Inventory Status`, async () => {
-    expect(inventory.getStatus()).toBeInstanceOf(InventoryStatus);
-  });
 });
 
 describe("Inventory works correctly ", () => {
-  test(`Should become Available`, async () => {
-    expect(inventory).toBeInstanceOf(Inventory);
-  });
-
-  test(`Should be able to get Inventory`, async () => {
-    expect(inventory.getStatus()).toBeInstanceOf(InventoryStatus);
+  test(`Should be able to get Inventory Entries`, async () => {
+    const entries = inventory.getEntries();
+    console.log(typeof status);
+    expect(entries.size).toBeGreaterThanOrEqual(0);
   });
 });
--- src/Resources/Inventory/Inventory.ts
+++ src/Resources/Inventory/Inventory.ts
@@ -1,1 +1,65 @@
-export class Inventory {}
+import { BehaviorSubject, Observable } from "rxjs";
+
+export class Inventory {
+  private entries = new Map<string, InventoryEntry>();
+  private state = new State();
+  public getStatus(): Status {
+    const status = this.state.status;
+    return status;
+  }
+  public getState(): State {
+    return this.state;
+  }
+  public getEntries(): Map<string, InventoryEntry> {
+    return this.entries;
+  }
+
+  constructor() {
+    this.state.status = Status.Available;
+  }
+}
+
+export enum Status {
+  Initializing = "Initializing",
+  Loading = "Loading",
+  Available = "Available",
+  Unavailable = "Unavailable"
+}
+
+export class State {
+  private statusSubject = new BehaviorSubject<Status>(Status.Initializing);
+  public get status$(): Observable<Status> {
+    return this.statusSubject.asObservable();
+  }
+
+  public get status(): Status {
+    return this.statusSubject.getValue() as Status;
+  }
+
+  public set status(status: Status) {
+    this.statusSubject.next(status);
+  }
+}
+
+export type InventoryEntry = {
+  ScanCode: string;
+  DefaultSupplier: string;
+  Department: string;
+  Brand: string;
+  Name: string;
+  Size: string;
+  ReceiptAlias: string;
+  BasePrice: string;
+  LastCost: string;
+  AverageCost: string;
+  SubDepartment: string;
+  IdealMargin: string;
+  Quantity: string;
+  Unit: string;
+  SupplierUnitID: string;
+  N: string;
+  S: string;
+  NorthLSD: string;
+  SouthLSD: string;
+  valuesArray: Array<string>;
+};
