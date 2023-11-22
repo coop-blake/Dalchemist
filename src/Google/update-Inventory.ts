f9de8fa85db3e85f15ca834cf57d98ce274310ce
coop-blake <96803255+coop-blake@users.noreply.github.com>
2023-11-21 17:48:12 -0800

    Adding pkg

--- src/Google/update-Inventory.ts
+++ src/Google/update-Inventory.ts
@@ -1,8 +1,8 @@
 import { google } from "googleapis";
 import { spawn } from "child_process";
 
-import fs from "fs";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
 
 import InventoryImporter from "../TextImporters/Inventory";
 
